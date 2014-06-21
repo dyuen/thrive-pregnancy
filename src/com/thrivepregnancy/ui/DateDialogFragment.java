47192ab76661cb9d630bad2165d651e2e93bcca3
Robert Thomas <git@robert-thomas.ca>
2014-06-21 10:38:49 -0700

    BaseActivity extends FragmentActivity Added layouts activity_startup, date_picker, list_item_tip Added Save menu Added DateDialogFragment, StartupActivity

---
+++ src/com/thrivepregnancy/ui/DateDialogFragment.java
@@ -0,0 +1,62 @@
+package com.thrivepregnancy.ui;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.thrivepregnancy.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.content.DialogInterface;
+import android.view.View;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+/**
+ * A Fragment encapsulating a AlertDialog containing a DatePicker. 
+ */
+public class DateDialogFragment extends DialogFragment{
+
+	private DatePicker 			datePicker;
+	private OnDateSetListener 	dateListener;
+	private AlertDialog			dialog;
+	
+	public static DateDialogFragment newInstance(String fragmentNumber, OnDateSetListener dateListener) {
+		DateDialogFragment newInstance = new DateDialogFragment();
+		newInstance.dateListener = dateListener;
+		Bundle args = new Bundle();
+		args.putString("fragnum", fragmentNumber);
+		newInstance.setArguments(args);
+		return newInstance;
+	}
+
+	@Override
+	public AlertDialog onCreateDialog(Bundle savedInstanceState) {
+		
+		Activity parentActivity = getActivity();	
+		datePicker = (DatePicker)parentActivity.getLayoutInflater().inflate(R.layout.date_picker, null);
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(parentActivity);
+		builder.setTitle("Title");		
+		builder.setView(datePicker);
+		// Pass the selected date back to the activity only when Save is clicked
+		builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dateListener.onDateSet(datePicker, datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());				
+			}
+		});
+		
+		return builder.create();
+	}
+	
+	private void configurePickerDates(){
+		Calendar now = Calendar.getInstance(); 
+		datePicker.setMinDate((now.getTime()).getTime());
+		now.add(Calendar.MONTH, 9);
+		datePicker.setMaxDate((now.getTime()).getTime());
+	}
+	
+}
---
+++ src/com/thrivepregnancy/ui/StartupActivity.java
@@ -0,0 +1,174 @@
+package com.thrivepregnancy.ui;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.DatePickerDialog.OnDateSetListener;
+
+import com.thrivepregnancy.R;
+
+/**
+ * Displays the registration screen for the user to enter name and due date.
+ * This is executed only upon a new install (but not an update)
+ */
+public class StartupActivity extends FragmentActivity implements OnDateSetListener{
+	
+	/**
+	 * Name of Preferences
+	 */
+	public static final String PREFERENCES = "preferences";
+	/**
+	 * Key to the user's name in the application Preferences
+	 */
+	public static final String PREFERENCE_NAME = "name";
+	/**
+	 * Key to the user's expected due date in the application Preferences
+	 * The date is saved as a long
+	 */
+	public static final String PREFERENCE_DUE_DATE = "date";
+	
+	private static SimpleDateFormat dateFormat = new SimpleDateFormat("MMMMMMMMM d, yyyy");
+
+	private String 				name = null;
+	private FragmentManager		fragmentManager;
+	private Calendar 			calendar;
+	private OnDateSetListener 	dateListener;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+        fragmentManager = getSupportFragmentManager();
+        dateListener = this;
+        
+        // If name has been set, proceed immediately to main screen
+    	SharedPreferences preferences = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+    	if (preferences.getString(PREFERENCE_NAME, null) != null){
+        	Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        	startActivity(intent);
+        	this.finish();
+    	}
+    	
+    	TextView dateView = (TextView)findViewById(R.id.startup_date);
+        dateView.setOnClickListener(new OnClickListener() {        
+            public void onClick(View v) {
+              	DateDialogFragment fragment = DateDialogFragment.newInstance("1", dateListener);
+               	fragment.show(fragmentManager, "1");
+            }
+        });
+    	
+    	/*
+    	 * Code below was an attempt to use an EditText for the date field, for visual 
+    	 * consistency with the name field. Not reliable, because Android can swallow 
+    	 * focus or click event to bring up keyboard
+    	 * 
+        EditText dateView = (EditText)findViewById(R.id.startup_date);
+        EditText nameView = (EditText)findViewById(R.id.startup_date);
+       
+        // When date field receives focus or clicked, show a date picker dialog
+        dateView.setOnFocusChangeListener(new OnFocusChangeListener() {        
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus && !fragmentShown){
+                	fragmentShown = true;
+                    DateDialogFragment fragment = DateDialogFragment.newInstance("1", dateListener);
+                    fragment.show(fragmentManager, "1");
+                }
+            }
+        });        
+        dateView.setOnClickListener(new OnClickListener() {        
+            public void onClick(View v) {
+            	if (!fragmentShown){
+            		fragmentShown = true;
+                	DateDialogFragment fragment = DateDialogFragment.newInstance("1", dateListener);
+                	fragment.show(fragmentManager, "1");
+            	}
+            }
+        });
+        // When name field receives focus, reset the flag
+        dateView.setOnFocusChangeListener(new OnFocusChangeListener() {        
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                	fragmentShown = false;
+                }
+            }
+        }); 
+        */       
+    }
+    
+    /**
+     * Called when the date has been set 
+     */
+	public void onDateSet(DatePicker view, int year, int month, int day) {
+		// Save the new date in the calendar
+		if (calendar == null){
+			calendar = Calendar.getInstance();
+			calendar.clear();        
+		}
+		calendar.set(Calendar.YEAR, year);		
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.DAY_OF_MONTH, day);
+        
+		// Update the display
+        TextView date = (TextView)findViewById(R.id.startup_date);
+        date.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    /**
+     * Called when the Save menu item is pressed
+     */
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.menu_save:
+	        	// Confirm that name and date are set. For confirming
+	        	// that a "full" name is entered, check its length
+	        	EditText nameView = (EditText)findViewById(R.id.startup_name);       
+	        	name = nameView.getText().toString();	        	
+	        	if (name != null && (name.length() > 5) && calendar != null){
+	        		// Save name and date in preferences
+	        		saveValues();
+	        		// Proceed to the main screen
+		        	Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+		        	startActivity(intent);
+	    		}
+	        	else {
+	        		// Remind user to enter both name and date
+		        	TextView textView = (TextView)findViewById(R.id.startup_warning);
+	        		textView.setText(R.string.startup_warning);
+	        	}
+	            return true;
+	        
+	        default:	        	
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+    
+    private void saveValues(){
+    	long dateInMillis = calendar.getTimeInMillis();
+    	SharedPreferences preferences = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+		SharedPreferences.Editor editor = preferences.edit();
+    	editor.putString(PREFERENCE_NAME, name);
+    	editor.putLong(PREFERENCE_DUE_DATE, dateInMillis);
+    	editor.commit();
+    }
+
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.save, menu);
+	    return true;
+	}		
+}
